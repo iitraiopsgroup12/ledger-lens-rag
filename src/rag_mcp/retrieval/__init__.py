75647f870ea9a8f000dc889f4757dc763b40116b
Vishal Soni <iitr.aiops.group.12@gmail.com>
2026-06-07 14:50:27 +0530

    Implemented FIAS vector DB

--- src/rag_mcp/retrieval/__init__.py
+++ src/rag_mcp/retrieval/__init__.py
@@ -3,6 +3,8 @@
 from rag_mcp.retrieval.base import Retriever
 from rag_mcp.retrieval.in_memory_store import InMemoryVectorStore, VectorStoreError
 from rag_mcp.retrieval.vector_store import Document, ScoredDocument, VectorStore
+from rag_mcp.retrieval.ingest import ingest_texts
+from rag_mcp.retrieval.faiss_store import FaissVectorStore, FaissError
 
 __all__ = [
     "Retriever",
@@ -11,5 +13,8 @@ __all__ = [
     "ScoredDocument",
     "InMemoryVectorStore",
     "VectorStoreError",
+    "ingest_texts",
+    "FaissVectorStore",
+    "FaissError",
 ]
 
